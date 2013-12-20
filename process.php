0c907a09c3161f9f5a00d2bdba59191210e1d99a
QingqiShi <qingqishi@live.com>
2013-12-20 20:26:17 +0000

    增加日期范围
    增加日期范围的选项，并且尝试加入算适当算法

--- process.php
+++ process.php
@@ -17,16 +17,16 @@
 		<form action="#" method="post">
 			<input type="hidden" name="name" value="<?php echo $name;?>">
 			<select name="frequency" class="name">
-				<option value="7"<?php if($frequency == 1) {echo " selected";}?>>过去7天</option>
-				<option value="24"<?php if($frequency == 24) {echo " selected";}?>>过去30天</option>
+				<option value="7"<?php if($frequency == 1) {echo " selected";}?>>当天 </option>
+				<option value="24"<?php if($frequency == 24) {echo " selected";}?>>每日</option>
 				<option value="144"<?php if($frequency == 144) {echo " selected";}?>>每周</option>
-				<option value="744"<?php if($frequency == 744) {echo " selected";}?>>过去360</option>
+				<option value="744"<?php if($frequency == 744) {echo " selected";}?>>每月</option>
 			</select>
             <select name="range" class="name">
-				<option value="1"<?php if($range == 1) {echo " selected";}?>>当天 </option>
-				<option value="7"<?php if($range == 24) {echo " selected";}?>>每日</option>
-				<option value="144"<?php if($range == 144) {echo " selected";}?>>过去90天</option>
-				<option value="744"<?php if($range == 744) {echo " selected";}?>>每月</option>
+				<option value="1"<?php if($range == 7) {echo " selected";}?>>过去7天</option>
+				<option value="7"<?php if($range == 30) {echo " selected";}?>>过去30天</option>
+				<option value="144"<?php if($range == 90) {echo " selected";}?>>过去90天</option>
+				<option value="744"<?php if($range == 365) {echo " selected";}?>>过去365天</option>
 			</select>
 			<input type="submit" value="拉取数据" class="submit" />
 		</form>
@@ -68,52 +68,53 @@
 		$loop = 11;
 	}
 
-	while ($i < 1000 && feof($file) == false) {
+	while (feof($file) == false) {
 		$record = rtrim(fgets($file));
 		$temp = substr($record, 0, strpos($record, " "));
-		if ($record != "" && date($timeStr, $temp) != $pre_set) {
-?>
-			<tr>
-				<td><?php 
-					echo date("n\月j\日 G\:i", $temp);
-					$pre_set = date($timeStr, $temp);
-				?></td>
-				<td><?php 
-					$temp = ltrim(substr($record, strpos($record, " ")));
-					$sub = substr($temp, 0, strpos($temp, " "));
-					echo $sub;
-				?></td>
-				<td><?php 
-					if ($pre_sub != 0) {
-						echo ($sub - $pre_sub);
-					}
-					$pre_sub = $sub;
-				?></td>
-				<td><?php 
-					$temp = ltrim(substr($temp, strpos($temp, " ")));
-					$view = substr($temp, 0, strpos($temp, " "));
-					echo $view;
-				?></td>
-				<td><?php 
-					if ($pre_view != 0) {
-						echo ($view - $pre_view);
-					}
-					$pre_view = $view;
-				?></td>
-				<td><?php 
-					$visit = substr($temp, strpos($temp, " ")+1);
-					echo $visit;
-				?></td>
-				<td><?php 
-					if ($pre_visit != 0) {
-						echo ($visit - $pre_visit);
-					}
-					$pre_visit = $visit;
-				?></td>
-			</tr>
-<?php
-			$i++;
-		}
+        if (time() - $temp <= $range * 24 * 60 * 60) {
+            if ($record != "" && date($timeStr, $temp) != $pre_set) {
+    ?>
+                <tr>
+                    <td><?php 
+                        echo date("n\月j\日 G\:i", $temp);
+                        $pre_set = date($timeStr, $temp);
+                    ?></td>
+                    <td><?php 
+                        $temp = ltrim(substr($record, strpos($record, " ")));
+                        $sub = substr($temp, 0, strpos($temp, " "));
+                        echo $sub;
+                    ?></td>
+                    <td><?php 
+                        if ($pre_sub != 0) {
+                            echo ($sub - $pre_sub);
+                        }
+                        $pre_sub = $sub;
+                    ?></td>
+                    <td><?php 
+                        $temp = ltrim(substr($temp, strpos($temp, " ")));
+                        $view = substr($temp, 0, strpos($temp, " "));
+                        echo $view;
+                    ?></td>
+                    <td><?php 
+                        if ($pre_view != 0) {
+                            echo ($view - $pre_view);
+                        }
+                        $pre_view = $view;
+                    ?></td>
+                    <td><?php 
+                        $visit = substr($temp, strpos($temp, " ")+1);
+                        echo $visit;
+                    ?></td>
+                    <td><?php 
+                        if ($pre_visit != 0) {
+                            echo ($visit - $pre_visit);
+                        }
+                        $pre_visit = $visit;
+                    ?></td>
+                </tr>
+    <?php
+            }
+        }
 	}
 ?>
 		</table>
