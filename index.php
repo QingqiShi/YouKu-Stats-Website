7d9caabf5be53129df1bfb6ee9b41e75cdaefd30
QingqiShi <qingqishi@live.com>
2013-12-22 21:45:46 +0000

    主页加入标题

--- index.php
+++ index.php
@@ -5,22 +5,23 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
+        <div class="title">优酷数据统计</div>
 		<div class="pull">
-		<form action="process.php" method="post">
-			<select name="name" class="name">
-				<?php
-					$file = fopen("./list.txt", "r");
-					while (!feof($file)) {
-						$temp = rtrim(fgets($file));
-						echo "<option value=\"".$temp."\">".$temp."</option>\n";
-					}
-					fclose($file);
-				?>
-			</select>
-			<input type="hidden" name="frequency" value="24">
-            <input type="hidden" name="range" value="30">
-			<input type="submit" value="拉取数据" class="submit"/>
-		</form>
+            <form action="process.php" method="post">
+                <select name="name" class="name">
+                    <?php
+                        $file = fopen("./list.txt", "r");
+                        while (!feof($file)) {
+                            $temp = rtrim(fgets($file));
+                            echo "<option value=\"".$temp."\">".$temp."</option>\n";
+                        }
+                        fclose($file);
+                    ?>
+                </select>
+                <input type="hidden" name="frequency" value="24">
+                <input type="hidden" name="range" value="30">
+                <input type="submit" value="拉取数据" class="submit"/>
+            </form>
 		</div>
 	</body>
 </html>					
