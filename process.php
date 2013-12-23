6ceed6a67845df6792579fb1e30d9c0d51730560
QingqiShi <qingqishi@live.com>
2013-12-23 13:20:26 +0000

    去除txt文件，更新css

--- process.php
+++ process.php
@@ -71,7 +71,10 @@
     while (feof($file) == false) {
 		$record = rtrim(fgets($file));
         $temp = substr($record, 0, strpos($record, " "));
-        if ((time() - $temp) <= ($range * 24 * 3600)) {
+        $tmphour = intval(date("G", $temp));
+        $tmpminutes = intval(date("i", $temp));
+        $tmpseconds = intval(date("s", $temp));
+        if ((time() - $temp + $tmphour * 3600 + $tmpminutes * 60 + $tmpseconds) <= ($range * 24 * 3600)) {
             break;
         }
         if ($record != "" && date($timeStr, $temp) != $pre_set) {
@@ -97,49 +100,47 @@
 	while (feof($file) == false) {
 		$record = rtrim(fgets($file));
 		$temp = substr($record, 0, strpos($record, " "));
-        if (time() - $temp <= $range * 24 * 60 * 60) {
-            if ($record != "" && date($timeStr, $temp) != $pre_set) {
-    ?>
-                <tr>
-                    <td><?php 
-                        echo date("n\月j\日 G\:i", $temp);
-                        $pre_set = date($timeStr, $temp);
-                    ?></td>
-                    <td><?php 
-                        $temp = ltrim(substr($record, strpos($record, " ")));
-                        $sub = substr($temp, 0, strpos($temp, " "));
-                        echo $sub;
-                    ?></td>
-                    <td><?php 
-                        if ($pre_sub != 0) {
-                            echo ($sub - $pre_sub);
-                        }
-                        $pre_sub = $sub;
-                    ?></td>
-                    <td><?php 
-                        $temp = ltrim(substr($temp, strpos($temp, " ")));
-                        $view = substr($temp, 0, strpos($temp, " "));
-                        echo $view;
-                    ?></td>
-                    <td><?php 
-                        if ($pre_view != 0) {
-                            echo ($view - $pre_view);
-                        }
-                        $pre_view = $view;
-                    ?></td>
-                    <td><?php 
-                        $visit = substr($temp, strpos($temp, " ")+1);
-                        echo $visit;
-                    ?></td>
-                    <td><?php 
-                        if ($pre_visit != 0) {
-                            echo ($visit - $pre_visit);
-                        }
-                        $pre_visit = $visit;
-                    ?></td>
-                </tr>
-    <?php
-            }
+        if ($record != "" && date($timeStr, $temp) != $pre_set) {
+?>
+            <tr>
+                <td><?php 
+                    echo date("n\月j\日 G\:i", $temp);
+                    $pre_set = date($timeStr, $temp);
+                ?></td>
+                <td><?php 
+                    $temp = ltrim(substr($record, strpos($record, " ")));
+                    $sub = substr($temp, 0, strpos($temp, " "));
+                    echo $sub;
+                ?></td>
+                <td><?php 
+                    if ($pre_sub != 0) {
+                        echo ($sub - $pre_sub);
+                    }
+                    $pre_sub = $sub;
+                ?></td>
+                <td><?php 
+                    $temp = ltrim(substr($temp, strpos($temp, " ")));
+                    $view = substr($temp, 0, strpos($temp, " "));
+                    echo $view;
+                ?></td>
+                <td><?php 
+                    if ($pre_view != 0) {
+                        echo ($view - $pre_view);
+                    }
+                    $pre_view = $view;
+                ?></td>
+                <td><?php 
+                    $visit = substr($temp, strpos($temp, " ")+1);
+                    echo $visit;
+                ?></td>
+                <td><?php 
+                    if ($pre_visit != 0) {
+                        echo ($visit - $pre_visit);
+                    }
+                    $pre_visit = $visit;
+                ?></td>
+            </tr>
+<?php
         }
 	}
 ?>
