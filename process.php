04dd5f6954fe182ad9960c6a3d841016a3af46a7
QingqiShi <qingqishi@live.com>
2014-01-06 17:45:14 +0000

    压缩输出

--- process.php
+++ process.php
@@ -1,3 +1,4 @@
+<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
 <?php
     date_default_timezone_set('PRC');
     $name = $_POST["name"];
