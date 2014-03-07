f8877af0630740c188e637c93cc8fb57d342287f
QingqiShi <qingqishi@live.com>
2014-03-07 22:02:32 +0000

    合并Jquery和自定JS文件

--- process.php
+++ process.php
@@ -34,7 +34,6 @@ if ($name != "") {
         <meta name=viewport content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src="jquery-2.0.3.min.js"></script>
-        <script type="text/javascript" src="script.js"></script>
         <script type="text/javascript" src="jscharts.js"></script>
     </head>
     <body>
